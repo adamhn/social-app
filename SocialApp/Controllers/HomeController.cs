0631c778c3c437a280e75ef0dde27cd3546bd0f8
Adam Hassan <adam.hn@icloud.com>
2020-12-17 21:38:13 +0100

    Update HomeController.cs

--- SocialApp/Controllers/HomeController.cs
+++ SocialApp/Controllers/HomeController.cs
@@ -19,12 +19,5 @@ namespace SocialApp.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
