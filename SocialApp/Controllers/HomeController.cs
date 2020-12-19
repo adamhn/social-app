48ff3463c841464886d411223376a73b46de6327
Adam Hassan <adam.hn@icloud.com>
2020-12-19 13:12:16 +0100

    Added current application user with display name

--- SocialApp/Controllers/HomeController.cs
+++ SocialApp/Controllers/HomeController.cs
@@ -1,21 +1,56 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using SocialApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace SocialApp.Controllers
 {
     public class HomeController : Controller
     {
+
+        /// <summary>
+        /// Application DB context
+        /// </summary>
+        protected ApplicationDbContext ApplicationDbContext { get; set; }
+
+        /// <summary>
+        /// User manager - attached to application DB context
+        /// </summary>
+        protected UserManager<ApplicationUser> UserManager { get; set; }
+
+        public HomeController()
+        {
+            this.ApplicationDbContext = new ApplicationDbContext();
+            this.UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(this.ApplicationDbContext));
+        }
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public async Task<ActionResult> About()
         {
-            ViewBag.Message = "Your application description page.";
+            ApplicationUser currentUser = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+
+            if (currentUser != null)
+            {
+                ViewBag.Message = "Hey " + currentUser.Firstname  + " " + currentUser.Lastname;
+                return View();
+            } 
+
+            ViewBag.Message = "Your application description page for /about. Hey anonymos";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your application description page for /contact.";
             return View();
         }
 
